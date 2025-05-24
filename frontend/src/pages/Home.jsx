5cb36e82fe5d8de878a7f9a6279d453abfd04b1c
LxxsCLN <luisxxcanada@gmail.com>
2025-05-24 00:27:29 -0600

    ajustar estilos para movil

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -120,20 +120,22 @@ const Home = () => {
                 {/* Header */}
                 <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
                     <div>
-                        <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+                        <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 text-center sm:text-start">
                             {t("home.title")}
                         </h1>
-                        <p className="text-gray-600 mt-1 dark:text-gray-300">
+                        <p className="text-gray-600 mt-1 dark:text-gray-300 text-center sm:text-start">
                             {t("home.description")}
                         </p>
                     </div>
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-4 w-full sm:w-auto justify-between">
                         <Button className="flex items-center gap-2">
                             <Plus className="h-4 w-4" />
                             {t("home.newTask")}
                         </Button>
-                        <LanguageToggle />
-                        <ThemeToggle />
+                        <div className="flex gap-4">
+                            <LanguageToggle />
+                            <ThemeToggle />
+                        </div>
                     </div>
                 </div>
 
